8a25bc51c9289713cfcd8f8236ccc8f262d9c901
Nick Debeurre <ndebeurr@asu.edu>
2019-05-23 16:39:44 -0700

    completed task 2

---
+++ Task 2/task2.js
@@ -0,0 +1,14 @@
+//Task on page 112 of 1264
+
+let message;
+let guess = prompt("How old am I?", "");
+
+if (guess < 23) {
+	message = "Too young!";
+} else if (age > 23) {
+	message = "Too old!";
+} else {
+	message = "You got it!";
+}
+
+alert(message);
