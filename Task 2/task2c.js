c8770ffd10f53a00d045c2510de23b563437622a
Nick Debeurre <ndebeurr@asu.edu>
2019-05-29 11:56:51 -0700

    finished task 2c

---
+++ Task 2/task2c.js
@@ -0,0 +1,20 @@
+// Task on page 113 of 1264
+
+//Turn this into a ? statement
+/*
+let a = prompt("Enter a value for a", "");
+let b = prompt("Enter a value for b", "");
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+alert(result);
+*/
+
+let a = prompt("Enter a value for a", "");
+let b = prompt("Enter a value for b", "");
+let result = (a + b < 4) ? 'Below' : 'Over';
+alert(result);
