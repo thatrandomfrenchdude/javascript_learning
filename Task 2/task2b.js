7c7b06dfae41b7d28614f4749a74370c5524ab79
Nick Debeurre <ndebeurr@asu.edu>
2019-05-29 11:48:52 -0700

    completed task 2b

--- Task 2/task2b.js
+++ Task 2/task2b.js
@@ -1,1 +1,14 @@
 // Task on page 113 of 1264
+
+let message;
+let val = prompt("Enter a number!", "");
+
+if (val < 0){
+  message = -1;
+} else if (val == 0) {
+  message = 0;
+} else {
+  message = 1;
+}
+
+alert(message);
