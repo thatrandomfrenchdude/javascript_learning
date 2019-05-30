f74bd1b7e954d80a2bb3e2befd1d07733a71ea64
Nick Debeurre <ndebeurr@asu.edu>
2019-05-29 17:39:09 -0700

    Task 3b completed

---
+++ Task3/task3b.js
@@ -0,0 +1,17 @@
+// Task on page 127 of 1264
+
+//Variant 1
+let age = prompt("Enter your age:", "");
+if (!(age >= 14 && age <= 90)) {
+  alert("True");
+} else {
+  alert("False");
+}
+
+//Variant 2
+let age = prompt("Enter your age:", "");
+if (age <= 14 || age >= 90) {
+  alert("True");
+} else {
+  alert("False")
+}
