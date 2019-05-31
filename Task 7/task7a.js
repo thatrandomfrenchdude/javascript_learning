fbeb7c6f61c96a3ee9b727638207a281b7446ba5
Nicholas Debeurre <32472100+thatrandomfrenchdude@users.noreply.github.com>
2019-05-31 14:03:13 -0700

    completed task 7

--- Task 7/task7a.js
+++ Task 7/task7a.js
@@ -1,3 +1,14 @@
-// Task on page
+// Task on page 194 of 1272
 
 "use strict";
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no()
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.");
+);
