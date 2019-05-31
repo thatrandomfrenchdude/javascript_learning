2537949c3bb09ab1082cd14c24ca9120e96ec36f
Nick Debeurre <ndebeurr@asu.edu>
2019-05-31 12:23:52 -0700

    Completed task 5

--- Task5/task5a.js
+++ Task5/task5a.js
@@ -1,3 +1,12 @@
-// Task on page
+// Task on page 156 of 1272
 
 "use strict";
+
+let input = prompt('Please enter a browser:', '');
+if (input == 'Edge') {
+  alert("You've got the Edge!");
+} else if (input == 'Chrome' || input == 'Firefox' || input == 'Safari' || input == 'Opera') {
+  alert('Okay we support these browsers too')
+} else {
+  alert('We hope that this page looks okay!')
+}
